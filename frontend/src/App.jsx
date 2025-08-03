eddac612c5a7f426a07a056d5038568d75fef677
swarndeep121 <129958502+swarndeep121@users.noreply.github.com>
2025-08-04 01:50:31 +0530

    removed all reruoting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,7 +38,7 @@ const App = () => {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/" />} />
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
