a338eccb87ff8c23c408af8f3c64bb7b1911abf8
swarndeep121 <129958502+swarndeep121@users.noreply.github.com>
2025-08-04 00:34:28 +0530

    PeaceMaker

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,16 +7,21 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
-  },
+    origin: [
+      "http://localhost:5173",                      // Local development
+      "https://fullstack-chat-app-76jx.onrender.com" // Production frontend on Render
+    ],
+    methods: ["GET", "POST"],
+    credentials: true
+  }
 });
+
+// Store online users: { userId: socketId }
+const userSocketMap = {};
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
-
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
@@ -24,7 +29,7 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
+  // Notify all clients of the updated online users list
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
